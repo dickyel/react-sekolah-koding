c78aac5e7077d7ccf1ccf24e8f8ceb7db03d5216
dickyel <45953381+dickyel@users.noreply.github.com>
2023-03-02 03:26:04 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -20,6 +20,82 @@ function Lanjutan(props) {
   return <span>Lanjutannya {props.kalimat} </span>;
 }
 
+// belajar state react js
+class Timer extends Component {
+  // ini merupakan fungsi constructor
+  constructor(props) {
+    // ini adalah nilai - nilai yang akan dimasukkan
+    super(props);
+    this.state = {
+      // kondisi awal dari state 0
+      waktu: props.start,
+    };
+  }
+
+  // lifecycle
+  ComponentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000);
+  }
+
+  ComponentWillUnMount() {
+    clearInterval(this.addInterval);
+  }
+
+  increase() {
+    // update state time diatas setiap detik
+    this.setState((state, props) => ({
+      // melakukan konvert ke dalam integer
+      waktu: parseInt(state.waktu) + 1,
+    }));
+  }
+
+  render() {
+    return <div>{this.state.waktu} detik</div>;
+  }
+}
+
+// buat event menggunakan fungsi
+function Clicker() {
+  function handleClick(e) {
+    alert('berhasil mengklik!');
+    e.preventDefault();
+  }
+
+  return (
+    <a href="" onClick={handleClick}>
+      Klik
+    </a>
+  );
+}
+
+class Toggle extends Component {
+  constructor(props) {
+    // ini adalah nilai - nilai yang akan dimasukkan
+    super(props);
+    this.state = {
+      // kondisi awal dari state 0
+      toggleStatus: true,
+    };
+
+    this.Click = this.Click.bind(this);
+  }
+
+  Click() {
+    this.setState((state) => ({
+      toggleStatus: !state.toggleStatus,
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.Click}>
+        <p>Kondisi Sekarang Masih {this.state.toggleStatus ? 'ON' : 'FALSE'}</p><br/>
+        <p>Lampu {this.state.toggleStatus ? 'Aktif' : 'mati'}</p>
+      </button>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -27,7 +103,7 @@ class App extends Component {
         {/* fungsi untuk memanggil komponen */}
         <Greeting />
         {/* fungsi untuk memanggil komponen dan properties */}
-        <Subtitle subs="start editing from to the zero;" kalimat="semangat" />
+        {/* <Subtitle subs="start editing from to the zero;" kalimat="semangat" />
         <Subtitle
           subs="jangankan sendiri aku pun bisa melakukannya;"
           kalimat="jangan menyerah"
@@ -35,7 +111,12 @@ class App extends Component {
         <Subtitle
           subs="tetap semangat dan jangan menyerah;"
           kalimat="tetap bersyukur"
-        />
+        /> */}
+
+        {/* DIPANGGIL DARI CLASS STATE IN THE TOP */}
+        <Timer start="0" /><br/>
+        <Clicker /><br/><br/>
+        <Toggle />
       </div>
     );
   }
